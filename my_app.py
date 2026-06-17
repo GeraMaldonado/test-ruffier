d1ba8a269b31851dcb898e3f52c38fb675f84b86
GeraMaldonado <gmaldonadofelix@hotmail.com>
2026-06-16 18:42:25 -0600

    creacion de constructor

--- my_app.py
+++ my_app.py
@@ -1,4 +1,18 @@
 from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout
 
 class MainWin(QWidget):
-  pass
+  def __init__(self):
+    super().__init__()
+    self.set_appear()
+    self.initUI()
+    self.connection()
+    self.show()
+
+  def set_appear(self):
+    pass
+
+  def initUI(self):
+    pass
+
+  def connection(self):
+    pass
