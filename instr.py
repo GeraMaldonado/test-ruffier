c3517833a84db44dab4f80a49915c86cccaa2ccd
GeraMaldonado <gmaldonadofelix@hotmail.com>
2026-06-16 18:50:41 -0600

    se añadieron el texto de la priemer ventana

--- instr.py
+++ instr.py
@@ -1,3 +1,14 @@
 txt_title = "Salud"
 win_x, win_y = 200, 100
 win_width, win_height = 1000, 600
+
+txt_hello = '¡Bienvenido al Programa de detección de estado de salud!'
+txt_next = 'Iniciar'
+txt_instruction = ('Esta aplicación le permite usar la prueba de Ruffier para realizar un diagnóstico inicial de su salud.\n'
+                   'La prueba de Ruffier es un conjunto de ejercicios físicos diseñado para evaluar su rendimiento cardíaco durante el esuferzo físico.\n'
+                   'El sujeto se tumba en posición supina durante 5 minutos y se toma la frecuencia del pulso durante 15 segundos; \n'
+                   'luego, dentro de 45 segundos, el sujeto realiza 30 sentadillas.\n'
+                   'Cuando el ejercicio termina, el sujeto se tumba y se toman sus pulsaciones de nuevo durante 15 segundos\n'
+                   'y luego durante los últimos 15 segundos del primer minuto del periodo de recuperación.\n'
+                   '¡Importante! Si no se siente bien durante la prueba (mareos,\n'
+                   'tinnitus, falta de respiración, etc.), detenga la prueba y consulte con un médico.' )
