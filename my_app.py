0f8475474586166a361b7455d2830a7ac45b0fdc
GeraMaldonado <gmaldonadofelix@hotmail.com>
2026-06-16 18:47:48 -0600

    Configuracion de texto en archivo separado

--- my_app.py
+++ my_app.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout
+from instr import txt_title, win_x, win_y, win_width, win_height
 
 class MainWin(QWidget):
   def __init__(self):
@@ -9,9 +10,9 @@ class MainWin(QWidget):
     self.show()
 
   def set_appear(self):
-    self.setWindowTitle('Test Ruffier')
-    self.resize(500,500)
-    self.move(300, 200)
+    self.setWindowTitle(txt_title)
+    self.resize(win_width, win_height)
+    self.move(win_x, win_y)
 
   def initUI(self):
     pass
